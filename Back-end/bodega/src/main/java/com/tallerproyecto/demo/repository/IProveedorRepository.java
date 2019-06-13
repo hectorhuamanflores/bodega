125207fb48716e0460e2d2511e58cd7e2604d3f1
hector <hector.10.fisi@gmail.com>
2019-06-13 01:07:33 -0500

    se modifico el crud - mejorado - data table ui

--- Back-end/bodega/src/main/java/com/tallerproyecto/demo/repository/IProveedorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tallerproyecto.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tallerproyecto.demo.model.Proveedor;
-
-public interface IProveedorRepository extends JpaRepository<Proveedor, Long>{
-
-}
--- Front-end/bodega/src/app/proveedores/proveedor.ts
+++
@@ -1,8 +0,0 @@
-export class Proveedor{
-    id: number;
-    nombre: string;
-    ruc: number;
-    direccion: string;
-    telefono: number;
-    email: string;
-}
